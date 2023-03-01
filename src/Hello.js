dc95b7e0e8df9417094cc989c25cb50a47080f43
Cactus <ysjungman@gmail.com>
2023-03-02 02:19:43 +0900

    리액트 eject

--- src/Hello.js
+++ src/Hello.js
@@ -1,12 +1,18 @@
 import React from 'react';
+// import RedColor from './atoms/Color'
+import styles from './Hello.module.css';
 
-const css = { // JavaScript 안에서 CSS  지정을 작성
-  color: 'red',
-  fontSize: 50,
-}
+
+// const css = { // JavaScript 안에서 CSS  지정을 작성
+//   color: 'red',
+//   fontSize: 50,
+// }
+
 
 const Hello = () => {
-  return <div style={css}>Hello, World!</div> //style 속성에 객체를 전달
+  
+  return <div className={styles.hello}>Hello, World!</div>
+  // return <div style={css}>Hello, World!</div> //style 속성에 객체를 전달
 }
 
 export default Hello
---
+++ src/atoms/Color.js
@@ -0,0 +1,8 @@
+let variableDate = 24 //javascript 변수
+
+const RedColor = {
+  color: 'red',
+  fontSize: variableDate //데이터 그대로 css에 적용할 수 있음
+}
+
+export default RedColor
